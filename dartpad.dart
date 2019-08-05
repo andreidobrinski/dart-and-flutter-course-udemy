27167d212133ff1d1fa085fb2713db69b8006cd4
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-08-05 09:47:28 -0400

    add notes about main function and types

--- dartpad.dart
+++ dartpad.dart
@@ -1,9 +1,12 @@
+// every Dart program starts by catching and running the main function
+// if no main function exists, error "Uncaught" will be shown
 void main() {
   var name = myName();
   
   print('My name is $name');
 }
 
+// "String" annotates return type
 String myName() {
   return 'Andrei';
 }
