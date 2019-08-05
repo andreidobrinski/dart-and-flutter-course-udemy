fe90ecf9f244b5213544489d56662cb1ff0b362e
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-08-05 10:22:34 -0400

    add constructor function for firstName argument

--- class-dartpad.dart
+++ class-dartpad.dart
@@ -1,14 +1,15 @@
 // "void" means that we are not returning anything from this function
 void main() {
-  var person = new Person();
-  
-  person.firstName = 'Andrei';
+  var person = new Person('Andrei');
   
   person.printName();
 }
 
 class Person {
   String firstName;
+  
+  // Dart assigns first argument to firstName on class
+  Person(this.firstName);
   
   printName() {
     // optional to use this.firstName in Dart
