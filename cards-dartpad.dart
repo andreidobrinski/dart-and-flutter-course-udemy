5347f11293a56395ca362f5b5c7105e748f3808a
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-08-06 14:30:10 -0400

    initialize Deck with Cards

--- cards-dartpad.dart
+++ cards-dartpad.dart
@@ -1,8 +1,26 @@
+void main() {
+  new Deck();
+}
+
 class Deck {
-  List<Card> cards;
+  List<Card> cards = [];
+  
+  Deck() {
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+    
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = new Card(rank, suit);
+        cards.add(card);
+      }
+    }
+  }
 }
 
 class Card {
   String suit;
   String rank;
+  
+  Card(this.rank, this.suit);
 }
