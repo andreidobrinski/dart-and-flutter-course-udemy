9775afe6168dbc0f43e946bbab090fedad26cc1f
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-08-07 22:35:21 -0400

    add scaffold for custom widget

---
+++ pics/lib/src/app.dart
@@ -0,0 +1,6 @@
+// import flutter helper library
+
+// create a class for the custom widget
+// class will extend 'StatelessWidget' base class
+
+// must define a build method that returns the widgets that this widget will show
