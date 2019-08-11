563c740b7769eb6c6a9251d4c2bd0ba04aa9953f
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-08-11 09:07:18 -0400

    add border around Image

--- pics/lib/src/widgets/image_list.dart
+++ pics/lib/src/widgets/image_list.dart
@@ -12,6 +12,10 @@ class ImageList extends StatelessWidget {
       itemBuilder: (context, int index) {
         return Container(
           margin: EdgeInsets.all(20.0),
+          padding: EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+          ),
           child: Image.network(images[index].url),
         );
       },
