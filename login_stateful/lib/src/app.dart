3d4b045dc81a64ec7eee70dc6b3b2db5a7b77117
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-08-11 09:39:12 -0400

    add LoginScreen to App

--- login_stateful/lib/src/app.dart
+++ login_stateful/lib/src/app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
 
 class App extends StatelessWidget {
   Widget build(context) {
     return MaterialApp(
       title: 'Login',
       home: Scaffold(
-        body: Text('Show a form here'),
+        body: LoginScreen(),
       ),
     );
   }
