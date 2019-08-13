85286a5fcf8f45550e6612a668a93e0c14d7de77
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-08-13 15:31:08 -0400

    add listen function for valid email

--- dartpad/streams-validation.dart
+++ dartpad/streams-validation.dart
@@ -4,18 +4,19 @@ import 'dart:async';
 void main() {
   final InputElement input = querySelector('input');
   final DivElement div = querySelector('div');
-  
-  final validator = new StreamTransformer.fromHandlers(
-    handleData: (inputValue, sink) {
-      if (inputValue.contains('@')) {
-        sink.add(inputValue);
-      } else {
-        sink.addError('Enter a valid email')
-      }
+
+  final validator =
+      new StreamTransformer.fromHandlers(handleData: (inputValue, sink) {
+    if (inputValue.contains('@')) {
+      sink.add(inputValue);
+    } else {
+      sink.addError('Enter a valid email');
     }
-  );
-  
+  });
+
   input.onInput
-    .map((dynamic event) => event.target.value)
-    .transform(validator)
+      .map((dynamic event) => event.target.value)
+      .transform(validator)
+      .listen((inputValue) => div.innerHtml = '',
+          onError: (err) => div.innerHtml = err);
 }
