dad7ea5a0f0a80bdf4a93e6720f3e35c2897c2ca
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-08-13 15:25:24 -0400

    add streams exercise on validation

---
+++ dartpad/streams-validation.dart
@@ -0,0 +1,8 @@
+import 'dart:html';
+
+void main() {
+  final InputElement input = querySelector('input');
+  final DivElement div = querySelector('div');
+
+  input.onInput.listen((dynamic event) => print(event.target.value));
+}
