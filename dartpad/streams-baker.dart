8272ca70c217a1732dd51fde28a753dc1897b500
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-08-13 14:40:49 -0400

    get type off of order

--- dartpad/streams-baker.dart
+++ dartpad/streams-baker.dart
@@ -1,17 +1,19 @@
 import 'dart:async';
 
 class Cake {}
-
 class Order {
   String type;
-
+  
   Order(this.type);
 }
 
 void main() {
   final controller = new StreamController();
-
+  
   final order = new Order('banana');
-
+  
   controller.sink.add(order);
+  
+  controller.stream
+    .map((order) => order.type)
 }
