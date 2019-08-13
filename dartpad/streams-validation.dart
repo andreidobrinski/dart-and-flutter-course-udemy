89a69b380662ee88a610bf66337bb85c0f69b4ea
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-08-13 15:28:35 -0400

    add StreamTransformer for validator

--- dartpad/streams-validation.dart
+++ dartpad/streams-validation.dart
@@ -1,8 +1,21 @@
 import 'dart:html';
+import 'dart:async';
 
 void main() {
   final InputElement input = querySelector('input');
   final DivElement div = querySelector('div');
-
-  input.onInput.listen((dynamic event) => print(event.target.value));
+  
+  final validator = new StreamTransformer.fromHandlers(
+    handleData: (inputValue, sink) {
+      if (inputValue.contains('@')) {
+        sink.add(inputValue);
+      } else {
+        sink.addError('Enter a valid email')
+      }
+    }
+  );
+  
+  input.onInput
+    .map((dynamic event) => event.target.value)
+    .transform(validator)
 }
