b758450e4bf01c0a2018638591116e09c07a73b0
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-09-02 22:56:46 -0400

    add combineLatest function for submitValid

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 // mixin "with" needs "extends" to work
 // also works with Bloc extends Validators
@@ -10,6 +11,8 @@ class Bloc extends Object with Validators {
   // Add data to stream
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _password.stream.transform(validatePassword);
+  Stream<bool> get submitValid =>
+      Observable.combineLatest2(email, password, (e, p) => true);
 
   // Change data
   Function(String) get changeEmail => _email.sink.add;
