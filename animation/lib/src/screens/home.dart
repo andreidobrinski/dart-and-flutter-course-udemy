4a373eb6521b62e879a0f40fec8ed94165b30126
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-11-20 17:14:03 -0500

    tweak animation values

--- animation/lib/src/screens/home.dart
+++ animation/lib/src/screens/home.dart
@@ -13,10 +13,10 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
     super.initState();
 
     catController = AnimationController(
-      duration: Duration(seconds: 2),
+      duration: Duration(milliseconds: 200),
       vsync: this,
     );
-    catAnimation = Tween(begin: -50.0, end: 100.0).animate(
+    catAnimation = Tween(begin: -35.0, end: -80.0).animate(
       CurvedAnimation(
         parent: catController,
         curve: Curves.easeIn,
