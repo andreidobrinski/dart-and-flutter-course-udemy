15b0dc645b6b220ba4ec341e7a7d71e5c43f9f07
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-12-03 16:10:00 -0500

    add fetchItem to db provider

--- news/lib/src/resources/news_db_provider.dart
+++ news/lib/src/resources/news_db_provider.dart
@@ -33,4 +33,19 @@ class NewsDbProvider {
         """);
     });
   }
+
+  fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    if (maps.length > 0) {
+      return ItemModel.fromDb(maps.first);
+    }
+
+    return null;
+  }
 }
