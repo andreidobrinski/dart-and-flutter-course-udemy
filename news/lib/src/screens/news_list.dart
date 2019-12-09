6fe2b1f194fd4d87ce0d74a43f03a8703a1afe7c
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-12-09 17:05:43 -0500

    add StreamBuilder to newslist

--- news/lib/src/screens/news_list.dart
+++ news/lib/src/screens/news_list.dart
@@ -5,11 +5,32 @@ class NewsList extends StatelessWidget {
   Widget build(context) {
     final bloc = StoriesProvider.of(context);
 
+    // temporary
+    bloc.fetchTopIds();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('list'),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Still waiting on Ids');
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            return Text(snapshot.data[index]);
+          },
+        );
+      },
     );
   }
 }
