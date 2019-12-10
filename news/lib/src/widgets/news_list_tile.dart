f1e397c6f7219e63529d333546b5075c375e839a
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-12-10 09:43:39 -0500

    more on NewsListTile

--- news/lib/src/widgets/news_list_tile.dart
+++ news/lib/src/widgets/news_list_tile.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import '../models/item_model.dart';
 import '../blocs/stories_provider.dart';
@@ -9,5 +10,14 @@ class NewsListTile extends StatelessWidget {
 
   Widget build(context) {
     final bloc = StoriesProvider.of(context);
+
+    return StreamBuilder(
+      stream: bloc.items,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Stream still loading');
+        }
+      },
+    );
   }
 }
