b61307359ca96c2a63776cc4d83d9fa7161f0147
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-12-11 16:40:15 -0500

    pass itemId to NewsDetail

--- news/lib/src/screens/news_detail.dart
+++ news/lib/src/screens/news_detail.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class NewsDetail extends StatelessWidget {
+  final int itemId;
+
+  NewsDetail({this.itemId});
+
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('detail'),
+        title: Text('Detail'),
       ),
-      body: Text('detail'),
+      body: Text('$itemId'),
     );
   }
 }
